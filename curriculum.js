f7e6ac2b1460438f4b25a60334d176bd4acd59e8
jampi2006 <jamphierorellana641@gmail.com>
2024-05-01 15:19:44 -0600

    agrege mas funciones y mas estilos

---
+++ curriculum.js
@@ -0,0 +1,7 @@
+let agregar2 = document.getElementById("agregar2")
+let tocame1 = document.getElementById("tocame1")
+let objeto = document.getElementById("objeto")
+
+tocame1.addEventListener("click",function(){
+objeto.innerHTML = agregar2.value
+})
